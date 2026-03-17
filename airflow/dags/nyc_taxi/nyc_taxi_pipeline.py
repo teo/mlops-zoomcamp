9634448df9dbef5d560dfbba55c23018e7194dcd
Teo Mrnjavac <teo.mrnjavac@gmail.com>
2026-03-17 17:03:06 +0100

    Use @task.virtualenv in Airflow DAG

--- airflow/dags/nyc_taxi/nyc_taxi_pipeline.py
+++ airflow/dags/nyc_taxi/nyc_taxi_pipeline.py
@@ -1,13 +1,6 @@
 from airflow.sdk import dag, task
 from pendulum import datetime
 
-from nyc_taxi.nyc_taxi_training import run
-
-
-@task
-def train_for_month(year: int, month: int) -> str:
-    return run(year, month)
-
 
 @dag(
     dag_id="nyc_taxi_training",
@@ -17,6 +10,21 @@ def train_for_month(year: int, month: int) -> str:
     tags=["mlops-zoomcamp", "homework"],
 )
 def nyc_taxi_training():
+    @task.virtualenv(
+        task_id="train_for_month",
+        requirements=[
+            "mlflow-skinny",
+            "pandas",
+            "pyarrow",
+            "scikit-learn",
+        ],
+        system_site_packages=False,
+    )
+    def train_for_month(year: int, month: int) -> str:
+        from nyc_taxi.nyc_taxi_training import run
+
+        return run(year, month)
+
     train_for_month(year=2023, month=3)
 
 
